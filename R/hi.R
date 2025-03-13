7da18656a963248ff3b83b832b0f760ac71a73ec
Mauro Lepore <maurolepore@gmail.com>
2025-03-13 13:41:45 +0000

    Document

--- R/hi.R
+++ R/hi.R
@@ -1,4 +1,11 @@
+#' Say hi
+#'
+#' @param name Character. A name.
+#' @return Character. A greeting.
 #' @export
+#' @examples
+#' hi()
+#' hi("me")
 hi <- function(name = NULL) {
   name <- name %||% "world"
   paste("Hello", name)
